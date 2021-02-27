c8c254c0d5e5e3338f97f68cd9ca7d018b20c551
Alexander Kim <alaki611@gmail.com>
2021-02-26 17:51:45 -0800

    Added fade in

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-
+import FadeIn from 'react-fade-in';
 import './Home.css';
 
 // Home page component (Landing page)
@@ -10,12 +9,14 @@ const HomePage = () => {
     return (
         <div>
             <Jumbotron className="jumbotronWrapper">
-                <h1 className="headerStyle mainTitle"> MaskCV </h1>
-                <h3 className="bodyStyle quoteText">"Wear a mask." - Dr. Anthony Fauci</h3>
-                <Button className="moreButton" href="#/About" size="lg" outline color="info">
-                    Explore
-                </Button>
-                <p className="bodyStyle addedText">Final project for CSE 455 taught by Joseph Redmon</p>
+                <FadeIn transitionDuration="1500" delay="80">
+                    <h1 className="headerStyle mainTitle"> MaskCV </h1>
+                    <h3 className="bodyStyle quoteText">"Wear a mask." - Dr. Anthony Fauci</h3>
+                    <Button className="moreButton" href="#/About" size="lg" outline color="info">
+                        Explore
+                    </Button>
+                    <p className="bodyStyle addedText">Final project for CSE 455 taught by Joseph Redmon</p>
+                </FadeIn>
             </Jumbotron>
         </div>
     );

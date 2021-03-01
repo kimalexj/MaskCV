09ae0b8fbfffa3fdba3345be55c32155d417b28d
Alexander Kim <alaki611@gmail.com>
2021-02-28 21:30:38 -0800

    Added credits

--- frontend/src/Credits/Credits.jsx
+++ frontend/src/Credits/Credits.jsx
@@ -1,7 +1,30 @@
+import { Col, Row, Container } from 'reactstrap';
+import './Credits.css';
+
 const CreditsPage = () => {
     return (
-        <div>
-            Credits
+        <div className="teamWrapper">
+            <Container>
+                <Row>
+                    <Col sm="6">
+                        <div className="creditWrapper">
+                            <h3>Team Members:</h3>
+                            <ul>
+                                <li>Alexander Kim</li>
+                                <li>Adnan Ahmad</li>
+                                <li>Angelito Somera</li>
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col sm="6">
+                        <h3>Preexisting Components:</h3>
+                        <ul>
+                            <li>VGG 16/19</li>
+                            <li>Haar Cascade Feature Classifier</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }

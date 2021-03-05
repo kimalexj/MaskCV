8108714bbacc31eba52ff4e6da02120c8605d39d
Angelito Somera <somera31@uw.edu>
2021-03-05 02:45:59 -0800

    added nose extraction

--- backend/model.py
+++ backend/model.py
@@ -6,43 +6,37 @@ print(cv2.__version__)
 # get cascade
 
 # DEFAULT CASCADE
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# USE CORRECT FILE PATH BEFORE UNCOMMENTING
-"""
-nose_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'nose.xml')
-mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'mouth.xml')
-"""
-
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+nose_cascade = cv2.CascadeClassifier('xml_cascades/nose.xml')
+mouth_cascade = cv2.CascadeClassifier('xml_cascades/mouth.xml')
 
 # CHANGE pass in image with correct pwd
-img = cv2.imread('/Users/adnanahmad/Desktop/test.jpg')
-
+img = cv2.imread('/Users/angelitosomera/Desktop/random.jpeg')
 img = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
-
-faces = face_cascade.detectMultiScale(img)
-
-# UNCOMMENT WHEN YOU FIX ABOVE
-""" 
-noses = nose_cascade.detectMultiScale(img)
-mouths = mouth_cascade.detectMultiScale(img);
-"""
-
 out_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
 
+# Faces
+faces = face_cascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=6, minSize=(40, 40))
 
 #plot boxes for faces
 for (x,y,w,h) in faces:
-    cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,0,255),1)
+    # cropped out faces (grayscaled and colored)
+    face = img[y:y+h, x:x+w]
+    out_face = out_img[y:y+h, x:x+w]
 
-# UNCOMMENT WHEN YOU FIX ABOVE
-"""
-for (x,y,w,h) in noses:
-    cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,0,255),1)
+    # Nose and Mouth extraction
+    # TODO: Fix mouth cascade from detecting eyes
+    nose = nose_cascade.detectMultiScale(face, scaleFactor=1.2, minNeighbors=6)
+    # mouth = mouth_cascade.detectMultiScale(face, scaleFactor=1.2, minNeighbors=6)
 
-for (x,y,w,h) in mouths:
-    cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,0,255),1)
-"""
+    # plot nose for current face
+    for (nx, ny, nw, nh) in nose:
+        cv2.rectangle(out_face, (nx, ny), (nx + nw, ny + nh), (0, 255, 0), 5)
+
+    # for (mx, my, mw, mh) in mouth:
+    #    cv2.rectangle(out_face, (mx, my), (mx + mw, my + mh), (255, 0, 0), 5)
+
+    cv2.rectangle(out_img, (x, y), (x + w, y + h), (0, 0, 255), 10)
 
 plt.imshow(out_img)
 plt.show()

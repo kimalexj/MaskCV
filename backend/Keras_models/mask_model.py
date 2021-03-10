c4a13a41b836892bd93a4c5881deaf571e1a45e0
Alexander Kim <alaki611@gmail.com>
2021-03-09 20:50:43 -0800

    optimized mask modeL

--- backend/Keras_models/mask_model.py
+++ backend/Keras_models/mask_model.py
@@ -11,10 +11,17 @@ from keras.preprocessing import image
 from keras import Sequential
 from keras.layers import Flatten, Dense, Dropout, LeakyReLU
 from keras.applications.vgg19 import VGG19
+from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
 import numpy as np
 import cv2
 
 def classifyImage(img):
+    # Current best = 95% testing accuracy on validation w/ 64 batch size
+
+    # Target model folder
+    saved_location = "./mask_model"
+
     # Predict on a single image (use openCV), will predict at the end
     mask_label = {0: 'Incorrect Mask', 1: 'With Mask', 2: 'Without Mask'}
     sample_image = img
@@ -25,7 +32,7 @@ def classifyImage(img):
 
     # Determine if model has already been trained (clean out mask_model to retrain)
     try:
-        model = keras.models.load_model('./mask_model') # folder for stored model
+        model = keras.models.load_model(saved_location) # folder for stored model
 
         # Predict for single image
         mask_result = model.predict(sample_image)
@@ -37,11 +44,32 @@ def classifyImage(img):
         testing_directory = './data/mask/Test';
 
         # Augment datasets
-        training_data_generator = ImageDataGenerator(rescale = 1./255,horizontal_flip=True, vertical_flip=True, zoom_range=0.3, shear_range=0.3);
-        train_generator = training_data_generator.flow_from_directory(directory=training_directory,target_size=(128,128),class_mode='categorical',batch_size=32)
+        training_data_generator = ImageDataGenerator(
+                    rescale = 1./255,
+                    horizontal_flip=True, 
+                    shear_range=0.3,
+                    height_shift_range = 0.2, 
+                    rotation_range = 10,
+                    zoom_range=0.3, 
+                    width_shift_range = 0.2);
+        train_generator = training_data_generator.flow_from_directory(
+                    directory=training_directory,target_size=(128,128),
+                    class_mode='categorical',
+                    batch_size=128);
 
-        testing_data_generator = ImageDataGenerator(rescale = 1./255,horizontal_flip=True, vertical_flip=True, zoom_range=0.3, shear_range=0.3);
-        test_generator = testing_data_generator.flow_from_directory(directory=testing_directory,target_size=(128,128),class_mode='categorical',batch_size=32)
+        testing_data_generator = ImageDataGenerator(
+                    rescale = 1./255,
+                    horizontal_flip=True, 
+                    shear_range=0.3,
+                    height_shift_range = 0.2, 
+                    rotation_range = 10,
+                    zoom_range=0.3, 
+                    width_shift_range = 0.2);
+        test_generator = testing_data_generator.flow_from_directory(
+                    directory=testing_directory,
+                    target_size=(128,128),
+                    class_mode='categorical',
+                    batch_size=64);
 
         # Test vgg19 model
         vgg19 = VGG19(weights='imagenet',include_top=False,input_shape=(128,128,3))
@@ -62,17 +90,37 @@ def classifyImage(img):
 
         print("Model compiled, ready for training\n");
 
+        # Stop training only when validation loss starts increasing (overfitting)
+        es = EarlyStopping(monitor='val_loss', patience=8, verbose=0, min_delta=0.001, mode='auto')
+
         history = model.fit(train_generator,
-                    steps_per_epoch=round(len(train_generator)/32),
+                    steps_per_epoch=4,
                     validation_data=test_generator,
-                    validation_steps=round(len(test_generator)/32),
-                    epochs=20) # Might want to go up to 20 epochs
+                    validation_steps=4,
+                    epochs=400,
+                    callbacks=[es])
+        
+        # Validation loss/accuracy
+        val_accuracy_history = history.history['val_accuracy']
+        val_loss_history = history.history['val_loss']
+        print("v_accuracy:") 
+        print(val_accuracy_history)
+        print("v_loss:")
+        print(val_loss_history)
+
+        # Training loss/accuracy
+        train_accuracy_history = history.history['accuracy']
+        train_loss_history = history.history['loss']
+        print("t_accuracy:")
+        print(train_accuracy_history)
+        print("t_loss:")
+        print(train_loss_history)
 
         results = model.evaluate(test_generator)
         print("test loss, test acc: \n", results)
 
         print("Finished training and evaluation, now saving model \n");
-        model.save('./mask_model')
+        model.save(saved_location)
 
         # Prediction
         print("Prediction for single image: ")

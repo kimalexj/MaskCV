4c53d581a4d0af4585891dd7518b516ae0ad66b4
Alexander Kim <alaki611@gmail.com>
2021-03-12 11:45:29 -0800

    embed video

--- frontend/src/Video/Video.jsx
+++ frontend/src/Video/Video.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
+import recording from './cse_455_final_project.mp4';
 
 const VideoPage = () => {
     return (
         <Jumbotron className="jumbotronWrapper">
-            Embed Video here :D
+            <video height="600" width="1000" controls>
+                <source src={recording} type="video/mp4" />
+            </video>
         </Jumbotron>
     );
 }
